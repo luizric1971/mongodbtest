dd50a9e0c0b64b01e59ffe8fd2da3560e174fee2
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-05-25 14:21:00 -0300

    User inserting with POST

--- src/main/java/com/javatest/mongodb/services/UserService.java
+++ src/main/java/com/javatest/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javatest.mongodb.domain.User;
+import com.javatest.mongodb.dto.UserDTO;
 import com.javatest.mongodb.repository.UserRepository;
 import com.javatest.mongodb.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 			Optional<User> obj = repository.findById(id);
 			
 			return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		}
+		
+		public User insert(User obj) {
+			return repository.insert(obj);
+		}
+		
+		public User fromDTO(UserDTO objDto) {
+			return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 		}
 }
