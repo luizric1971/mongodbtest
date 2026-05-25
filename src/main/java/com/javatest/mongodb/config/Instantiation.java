270d6949b36bf13f4f371d593be1134ca57e432a
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-05-25 20:21:26 -0300

    Adding comments to posts

--- src/main/java/com/javatest/mongodb/config/Instantiation.java
+++ src/main/java/com/javatest/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.javatest.mongodb.domain.Post;
 import com.javatest.mongodb.domain.User;
 import com.javatest.mongodb.dto.AuthorDTO;
+import com.javatest.mongodb.dto.CommentDTO;
 import com.javatest.mongodb.repository.PostRepository;
 import com.javatest.mongodb.repository.UserRepository;
 
@@ -22,25 +23,34 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		postRepository.deleteAll();
 		userRepository.deleteAll();
-		
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(u1));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(u1));
+
+		CommentDTO c1 = new CommentDTO("Boa Viagem Mano!", sdf.parse("21/03/2018"), new AuthorDTO(u2));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(u3));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(u2));
+
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
-		
+
 		u1.getPosts().addAll(Arrays.asList(p1, p2));
 		userRepository.save(u1);
 	}
