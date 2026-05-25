98caf316dc3c68ab4d6df62b8ad90e8a7ba7a65f
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-05-25 19:48:19 -0300

    endpoint to return posts of user

--- src/main/java/com/javatest/mongodb/resources/UserResource.java
+++ src/main/java/com/javatest/mongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.javatest.mongodb.domain.Post;
 import com.javatest.mongodb.domain.User;
 import com.javatest.mongodb.dto.UserDTO;
 import com.javatest.mongodb.services.UserService;
@@ -66,4 +67,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
