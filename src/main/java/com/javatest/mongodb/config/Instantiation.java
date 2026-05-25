1474250824dbc56536bcdaf0a7357ee9c5048d14
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-05-25 09:26:30 -0300

    Database instantiation

---
+++ src/main/java/com/javatest/mongodb/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.javatest.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.javatest.mongodb.domain.User;
+import com.javatest.mongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+	}
+
+}
